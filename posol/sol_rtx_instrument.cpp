d49b1797683381046452eea6518ae9c63a0f483e
gralkapk <patrick.gralka@informatik.uni-stuttgart.de>
2023-07-31 11:34:55 +0200

    wip sol_rtx_instrument

--- posol/sol_rtx_instrument.cpp
+++ posol/sol_rtx_instrument.cpp
@@ -9,16 +9,32 @@
 void visus::power_overwhelming::sol_rtx_instrument(void* state) {
     sol::state_view lua(reinterpret_cast<lua_State*>(state));
 
-    auto rtx_instrument_table = lua.new_usertype<rtx_instrument>(
-        "rtx_instrument", sol::constructors<rtx_instrument(),
-                              rtx_instrument(_In_z_ const char* path, _In_ const visa_instrument::timeout_type timeout),
-                              rtx_instrument(_Out_ std::reference_wrapper<bool>, _In_z_ const char*,
-                                  _In_ const visa_instrument::timeout_type)>());
-
+    auto rtx_instrument_table = lua.new_usertype<rtx_instrument>("rtx_instrument",
+        sol::constructors<rtx_instrument(),
+            rtx_instrument(const char* path, const visa_instrument::timeout_type timeout),
+            rtx_instrument(std::reference_wrapper<bool>, const char*, const visa_instrument::timeout_type)>());
+#if 0
     rtx_instrument_table["acquisition"] =
         sol::overload(static_cast<rtx_instrument& (rtx_instrument::*)(_In_ const oscilloscope_single_acquisition&,
                           _In_ const bool, _In_ const bool)>(&rtx_instrument::acquisition),
             static_cast<oscilloscope_acquisition_state (rtx_instrument::*)() const>(&rtx_instrument::acquisition)/*,
             static_cast<const rtx_instrument& (rtx_instrument::*)(_In_ const oscilloscope_acquisition_state,
                 _In_ const bool) const>(&rtx_instrument::acquisition)*/);
+#endif
+
+    rtx_instrument_table["acquisition"] =
+        static_cast<rtx_instrument& (rtx_instrument::*)(const oscilloscope_single_acquisition&, const bool,
+            const bool)>(&rtx_instrument::acquisition);
+
+    rtx_instrument_table["channel"] =
+        static_cast<rtx_instrument& (rtx_instrument::*)(const oscilloscope_channel&)>(&rtx_instrument::channel);
+
+    rtx_instrument_table["reference_position"] =
+        static_cast<rtx_instrument& (rtx_instrument::*)(const oscilloscope_reference_point)>(
+            &rtx_instrument::reference_position);
+
+    rtx_instrument_table["trigger_position"] = &rtx_instrument::trigger_position;
+
+    rtx_instrument_table["trigger"] =
+        static_cast<rtx_instrument& (rtx_instrument::*)(const oscilloscope_trigger&)>(&rtx_instrument::trigger);
 }
